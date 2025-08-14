4c8e4863d73ee28fee3106091cb026f480485788
BreadPetya <breadpetyaunpan@gmail.com>
2025-08-14 21:10:41 +0500

    Update install_from_git.py

--- install_from_git.py
+++ install_from_git.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 
 # Установка пакета из GitHub
-repo_url = "https://github.com/nigredon1991/tmp-uv-proj.git"
+repo_url = "https://github.com/BreadPetya/tmp-uv-proj.git"
 subprocess.check_call([
     sys.executable,
     "-m",
